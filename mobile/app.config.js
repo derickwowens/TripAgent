99d366b7f6730578c813254003d91482c1b10249
Derick Owens <derickwowens@Dericks-MBP.lan>
2026-01-12 12:46:14 -0600

    Bump version to 1.0.1 and versionCode to 2

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -5,7 +5,7 @@ export default ({ config }) => {
     ...config,
     name: "TripAgent",
     slug: "tripagent",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "automatic",
@@ -21,7 +21,7 @@ export default ({ config }) => {
     },
     android: {
       package: "com.tripagent.app",
-      versionCode: 1,
+      versionCode: 2,
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#166534"
