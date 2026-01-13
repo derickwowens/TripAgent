97f442552598a9e7b1d25c5ac77fe14e24e1fbaa
Derick Owens <derickwowens@Dericks-MBP.lan>
2026-01-12 21:13:23 -0600

    Bump version to 1.0.8 (versionCode: 9)

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -5,7 +5,7 @@ export default ({ config }) => {
     ...config,
     name: "TripAgent",
     slug: "tripagent",
-    version: "1.0.7",
+    version: "1.0.8",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "automatic",
@@ -21,7 +21,7 @@ export default ({ config }) => {
     },
     android: {
       package: "com.tripagent.app",
-      versionCode: 8,
+      versionCode: 9,
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#166534"
