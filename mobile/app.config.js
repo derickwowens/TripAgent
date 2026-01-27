a82ea52e04266773b43178ec32a72bbfb9e81f51
Derick Owens <derickwowens@Dericks-MBP.lan>
2026-01-27 12:15:56 -0600

    fix: sync app.config.js with app.json - version 1.9.5, versionCode 48

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -5,7 +5,7 @@ export default ({ config }) => {
     ...config,
     name: "TripAgent",
     slug: "tripagent",
-    version: "1.9.4",
+    version: "1.9.5",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "automatic",
@@ -29,7 +29,7 @@ export default ({ config }) => {
     },
     android: {
       package: "com.tripagent.app",
-      versionCode: 47,
+      versionCode: 48,
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#166534"
