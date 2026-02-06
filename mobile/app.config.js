ae6cfa3dfa2d6c74fed838af44d69c67a22aa053
Derick Owens <derickwowens@Dericks-MBP.lan>
2026-02-06 09:13:45 -0600

    Bump version to 2.5.1 (versionCode: 62)

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -5,7 +5,7 @@ export default ({ config }) => {
     ...config,
     name: "TripAgent",
     slug: "tripagent",
-    version: "2.5.0",
+    version: "2.5.1",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "automatic",
@@ -29,7 +29,7 @@ export default ({ config }) => {
     },
     android: {
       package: "com.tripagent.app",
-      versionCode: 61,
+      versionCode: 62,
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#166534"
